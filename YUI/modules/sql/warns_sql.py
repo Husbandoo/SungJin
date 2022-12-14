ac37e2c374a20ee2b39ca54eec8da84cb0a2c098
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 20:08:53 +0530

    Update warns_sql.py

--- YUI/modules/sql/warns_sql.py
+++ YUI/modules/sql/warns_sql.py
@@ -3,7 +3,7 @@ import threading
 from sqlalchemy import Integer, Column, String, UnicodeText, func, distinct, Boolean
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.sql.sqltypes import BigInteger
-from TOGA.modules.sql import SESSION, BASE
+from YUI.modules.sql import SESSION, BASE
 
 
 class Warns(BASE):
