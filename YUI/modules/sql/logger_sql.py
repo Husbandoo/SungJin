b6f2de76010dfb72b820dfc18f09630331e080c9
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 20:03:43 +0530

    Update logger_sql.py

--- YUI/modules/sql/logger_sql.py
+++ YUI/modules/sql/logger_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from TOGA.modules.sql import BASE, SESSION
+from YUI.modules.sql import BASE, SESSION
 
 class LoggerSettings(BASE):
     __tablename__ = "chat_log_settings"
