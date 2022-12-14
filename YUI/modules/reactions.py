7fb19c83e3dc4c6583db5a744a7df1f89537b706
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 22:39:37 +0530

    Update reactions.py

--- YUI/modules/reactions.py
+++ YUI/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from TOGA import dispatcher
-from TOGA.modules.disable import DisableAbleCommandHandler
+from YUI import dispatcher
+from YUI.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
