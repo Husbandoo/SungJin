7129c44a9faa4972734382926268f9f54f00d10a
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 22:49:18 +0530

    Update tts-stt.py

--- YUI/modules/tts-stt.py
+++ YUI/modules/tts-stt.py
@@ -7,9 +7,9 @@ import json
 from telegram import ChatAction
 from telegram.ext import run_async
 
-from TOGA import dispatcher
-from TOGA.modules.disable import DisableAbleCommandHandler
-from TOGA.modules.helper_funcs.alternate import typing_action, send_action
+from YUI import dispatcher
+from YUI.modules.disable import DisableAbleCommandHandler
+from YUI.modules.helper_funcs.alternate import typing_action, send_action
 
 @send_action(ChatAction.RECORD_AUDIO)
 def gtts(update, context):
