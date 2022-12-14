910310d5a42b1816d758b3cd846ff68f8d3bbc51
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 22:41:15 +0530

    Update remote_cmds.py

--- YUI/modules/remote_cmds.py
+++ YUI/modules/remote_cmds.py
@@ -1,8 +1,8 @@
-from TOGA import dispatcher
-from TOGA.modules.helper_funcs.chat_status import (
+from YUI import dispatcher
+from YUI.modules.helper_funcs.chat_status import (
     bot_admin, is_bot_admin, is_user_ban_protected, is_user_in_chat)
-from TOGA.modules.helper_funcs.extraction import extract_user_and_text
-from TOGA.modules.helper_funcs.filters import CustomFilters
+from YUI.modules.helper_funcs.extraction import extract_user_and_text
+from YUI.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
