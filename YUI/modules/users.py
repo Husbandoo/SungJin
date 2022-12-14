e8e1eb0dc854a11f013548b21fe68e4aeac072e3
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 19:18:42 +0530

    Update users.py

--- YUI/modules/users.py
+++ YUI/modules/users.py
@@ -12,11 +12,11 @@ from telegram.ext import (
     CommandHandler,
 )
 
-import TOGA.modules.sql.users_sql as sql
-from TOGA.modules.disable import DisableAbleCommandHandler
-from TOGA import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from TOGA.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from TOGA.modules.sql.users_sql import get_all_users
+import YUI.modules.sql.users_sql as sql
+from YUI.modules.disable import DisableAbleCommandHandler
+from YUI import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from YUI.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from YUI.modules.sql.users_sql import get_all_users
 
 
 USERS_GROUP = 4
