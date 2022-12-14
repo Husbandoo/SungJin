9be829e3a53207a5fb8f3b1b1cbe8ba4724a0a52
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 22:48:27 +0530

    Update translate.py

--- YUI/modules/translate.py
+++ YUI/modules/translate.py
@@ -1,8 +1,8 @@
 from telegram import  Update, ParseMode 
 from telegram.ext import run_async ,CallbackContext
 from gpytranslate import SyncTranslator
-from TOGA import dispatcher
-from TOGA.modules.disable import DisableAbleCommandHandler
+from YUI import dispatcher
+from YUI.modules.disable import DisableAbleCommandHandler
 trans = SyncTranslator()
  
 def totranslate(update: Update, context: CallbackContext) -> None:
