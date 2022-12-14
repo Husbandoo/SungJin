556525578f6b226a357949d725348150a07b2954
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 22:57:36 +0530

    Update chatstatus.py

--- YUI/modules/helper_funcs/telethn/chatstatus.py
+++ YUI/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from TOGA.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from TOGA import DRAGONS
+from YUI.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from YUI import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
