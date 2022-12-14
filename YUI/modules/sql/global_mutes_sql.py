28e23fa7accc087aee9ca6acd305e7c8c7818c94
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 20:01:50 +0530

    Update global_mutes_sql.py

--- YUI/modules/sql/global_mutes_sql.py
+++ YUI/modules/sql/global_mutes_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from TOGA.modules.sql import BASE, SESSION
+from YUI.modules.sql import BASE, SESSION
 
 
 class GloballyMutedUsers(BASE):
