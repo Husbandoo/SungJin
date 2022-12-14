d69bfb1ad8ec2763491c2c0a6e41833f6e08c8ff
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 19:54:37 +0530

    Update antiflood_sql.py

--- YUI/modules/sql/antiflood_sql.py
+++ YUI/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, BigInteger, UnicodeText
 
-from TOGA.modules.sql import SESSION, BASE
+from YUI.modules.sql import SESSION, BASE
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
