b4ae82f3cc48b39520b02893a056f65c1ff986a9
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 19:48:10 +0530

    Update __init__.py

--- YUI/modules/sql/__init__.py
+++ YUI/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from TOGA import DB_URI
+from YUI import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
