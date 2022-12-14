4086e8bd739dc883eff95f137580a276733961d3
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 22:45:00 +0530

    Update shell.py

--- YUI/modules/shell.py
+++ YUI/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from TOGA import LOGGER, dispatcher
-from TOGA.modules.helper_funcs.chat_status import dev_plus
+from YUI import LOGGER, dispatcher
+from YUI.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
