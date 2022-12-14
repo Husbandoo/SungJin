bf7f2710a5c414a939c5ace136b0cc98513862f7
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 17:40:46 +0530

    Update mentionsall.py

--- YUI/modules/mentionsall.py
+++ YUI/modules/mentionsall.py
@@ -7,7 +7,7 @@ from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.errors import UserNotParticipantError
 
-from TOGA import telethn as Client
+from YUI import telethn as Client
 
 spam_chats = []
 
