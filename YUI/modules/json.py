37edd48860355f4293fd30556a139f5ef5e927e3
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 22:26:17 +0530

    Update json.py

--- YUI/modules/json.py
+++ YUI/modules/json.py
@@ -6,8 +6,8 @@ from telethon import types
 from telethon.tl import functions, types
 
 
-from TOGA.events import register
-from TOGA import telethn
+from YUI.events import register
+from YUI import telethn
 
 
 async def is_register_admin(chat, user):
