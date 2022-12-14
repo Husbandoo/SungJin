550b80b3bd73b23f9066e491a0c8706d6fb96e72
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-14 17:38:32 +0530

    Update and rename kac.py to Zoro/kurumi.py

--- Zoro/kurumi.py
+++ Zoro/kurumi.py
@@ -1,10 +1,10 @@
-from TOGA import MONGO_DB_URI
+from YUI import MONGO_DB_URI
 from typing import Dict, List, Union
 from pymongo import MongoClient
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
-db = client["TOGA"]
+db = client["YUI"]
 
 coupledb = db.couple
 karmadb = db.karma
