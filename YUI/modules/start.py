b641a7f032b01504cceaf081ba1841b86e3af344
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-15 12:42:58 +0530

    Create start.py

---
+++ YUI/modules/start.py
@@ -0,0 +1,18 @@
+from pyrogram.types import Message
+from pyrogram import filters
+from YUI import pgram
+
+START_VID = "https://telegra.ph/file/2ddc15125fe6d1cd1b6fc.mp4"
+PM_START_TEXT = """
+*Kᴏɴɪᴄʜɪᴡᴀ {},*
+*I'ᴍ Yᴜɪɢᴀʜᴀᴍᴀ Yᴜɪ, A Aɴɪᴍᴇ Tʜᴇᴍᴇᴅ Gʀᴏᴜᴘ Mᴀɴᴀɢᴇᴍᴇɴᴛ Bᴏᴛ.*
+➖➖➖➖➖➖➖➖➖➖➖➖➖
+❍ *Uᴘᴛɪᴍᴇ:* {}
+❍ *Oᴡɴᴇʀ:* {OWNER_USERNAME}
+➖➖➖➖➖➖➖➖➖➖➖➖➖
+*Hɪᴛ Tʜᴇ /help Tᴏ Gᴇᴛ Lɪsᴛ Oғ Mʏ Cᴏᴍᴍᴀɴᴅs.××*
+"""
+
+@pgram.on_message(filters.command("start") filters.private)
+async def start(_, message):
+   await message.reply_video(START_VID , PM_START_TEXT)
